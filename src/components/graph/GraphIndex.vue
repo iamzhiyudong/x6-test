6810c48b2e52e02282e8fb25c55ef4184aab4ab8
zhiyudong <yudong.zhi@chaitin.com>
2023-06-28 17:27:40 +0800

    feat: 初始化项目，加 less，去除无用文件和配置;

---
+++ src/components/graph/GraphIndex.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>Graph</main>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped></style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <GraphIndex />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import GraphIndex from '../components/graph/GraphIndex.vue'
+</script>
+
+<style lang="less" scoped></style>
